7062b84bd91c96198ddfb35add4816a9901424f6
shirlenelss <shirlene_lss@yahoo.com>
2025-08-06 15:35:29 +0200

    add dummy test

--- src/test/java/com/example/react_gametime/ReactGametimeApplicationTests.java
+++ src/test/java/com/example/react_gametime/ReactGametimeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReactGametimeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
