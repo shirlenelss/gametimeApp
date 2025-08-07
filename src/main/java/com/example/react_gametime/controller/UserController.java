d34cf163f461896b3ae02ff64e432fdc849a831f
shirlenelss <shirlene_lss@yahoo.com>
2025-08-07 18:15:54 +0200

    added getUserByName to get user

--- src/main/java/com/example/react_gametime/controller/UserController.java
+++ src/main/java/com/example/react_gametime/controller/UserController.java
@@ -6,12 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/users-by-name/{userName}")
+    public ResponseEntity<User> addUser(@PathVariable String userName) {
+        Optional<User> user = userService.findByUsername(userName);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
     @PostMapping("/users")
     public ResponseEntity<User> addUser(@RequestBody User user) {
--- src/main/java/com/example/react_gametime/service/UserService.java
+++ src/main/java/com/example/react_gametime/service/UserService.java
@@ -20,7 +20,7 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
-    Optional<User> findByUsername(String username) {
+    public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 }
